1d495898a1c2243e437c3107f9517c1af884ce52
Andrei <betoxd2003@gmail.com>
2024-02-29 20:22:21 -0400

    Clase sesion 05

---
+++ sesion05.py
@@ -0,0 +1,62 @@
+print(type(1))
+
+variable = 100;
+print(variable);
+print(type(variable));
+
+# Precisión de 17 decimales
+variable_5 = 0.9999999999999999
+print(variable_5)
+print ( type (variable_5) )
+
+# Valor 2.0e-3 Flotante
+variable_6 = 2.0e-3
+print(variable_6)
+print ( type (variable_6) )
+
+# Operadores aritmeticos
+a = 10
+b = 3
+# Suma
+print("Suma")
+print(a+b)
+#Resta
+print("Resta")
+print(a - b);
+#Multiplicacion
+print("Multiplicacion")
+print(a * b);
+#Division
+print("Division")
+print(a / b);
+#Potencia
+print("Potencia")
+print(a ** b)
+#Modulo o residuo
+print("Modulo o residuo")
+print(a % b)
+#Division entera
+print("Division entera")
+print(a // b)
+
+cont = (300 + (3600 / 60)) / 60
+print("Horas: ", cont);
+
+print ("Operadores de comparación")
+comparar = 10
+print (comparar < 10)
+print (comparar > 10)
+print (comparar == 10)
+print (comparar <= 10)
+print (comparar >= 10)
+print (comparar != 10)
+
+print ("Operadores de comparación int - float")
+entero = 10
+flotante = 10.0
+print (entero < flotante)
+print (entero > flotante)
+print (entero == flotante)
+print (entero <= flotante)
+print (entero >= flotante)
+print (entero != flotante)
