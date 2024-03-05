82ccc06d56b74d410370fc0a77fbe5858966b287
AASG <betoxd2003@gmail.com>
2024-03-05 18:44:35 -0400

    arreglando ejercicio 3

--- tarea_sesion_05/ejercicio3.py
+++ tarea_sesion_05/ejercicio3.py
@@ -7,8 +7,8 @@ segundos_dados = 288325;
 segundos = segundos_dados % 60;
 minutos = segundos_dados // 60;
 horas = minutos // 60;
+dias = horas // 24;
 minutos = minutos % 60;
-dias = horas // 60;
-horas = horas % 60;
+horas = horas % 24;
 print("{} corresponde a: {} dia, {} horas, {} minutos, {} segundos"
       .format(segundos_dados, dias, horas, minutos, segundos));
